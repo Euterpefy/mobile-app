327fe85f48474158b7128ce31a0c26367642c654
bluesimp1102 <jackismyname7@gmail.com>
2024-02-18 23:53:53 -0800

    some colors

---
+++ lib/utils/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const red = Color.fromARGB(255, 230, 57, 71);
+const blue = Color.fromARGB(255, 124, 157, 255);
+const greenLightSea = Color.fromARGB(255, 23, 195, 178);
+const yellowSunset = Color.fromARGB(255, 255, 203, 119);
+const whiteFloral = Color.fromARGB(255, 254, 249, 239);
