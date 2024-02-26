371c5d718e6729448bcc8c4143c1ec1a017e4aff
bluesimp1102 <jackismyname7@gmail.com>
2024-02-26 15:46:42 -0800

    TrackRequest: updateable fields

--- lib/models/tracks_request.dart
+++ lib/models/tracks_request.dart
@@ -1,51 +1,53 @@
+import 'package:euterpefy/views/tracks_generating/advanced_criterias.dart';
+
 class TracksRequest {
-  final List<String> seedGenres;
-  final List<String> seedArtists;
-  final List<String> seedTracks;
-  final int limit;
-  final String? market;
-  final double? minAcousticness;
-  final double? maxAcousticness;
-  final double? targetAcousticness;
-  final double? minDanceability;
-  final double? maxDanceability;
-  final double? targetDanceability;
-  final int? minDurationMs;
-  final int? maxDurationMs;
-  final int? targetDurationMs;
-  final double? minEnergy;
-  final double? maxEnergy;
-  final double? targetEnergy;
-  final double? minInstrumentalness;
-  final double? maxInstrumentalness;
-  final double? targetInstrumentalness;
-  final int? minKey;
-  final int? maxKey;
-  final int? targetKey;
-  final double? minLiveness;
-  final double? maxLiveness;
-  final double? targetLiveness;
-  final double? minLoudness;
-  final double? maxLoudness;
-  final double? targetLoudness;
-  final int? minMode;
-  final int? maxMode;
-  final int? targetMode;
-  final int minPopularity;
-  final int maxPopularity;
-  final int? targetPopularity;
-  final double? minSpeechiness;
-  final double? maxSpeechiness;
-  final double? targetSpeechiness;
-  final double? minTempo;
-  final double? maxTempo;
-  final double? targetTempo;
-  final int? minTimeSignature;
-  final int? maxTimeSignature;
-  final int? targetTimeSignature;
-  final double? minValence;
-  final double? maxValence;
-  final double? targetValence;
+  late List<String> seedGenres;
+  late List<String> seedArtists;
+  late List<String> seedTracks;
+  late int limit;
+  late String? market;
+  late double? minAcousticness;
+  late double? maxAcousticness;
+  late double? targetAcousticness;
+  late double? minDanceability;
+  late double? maxDanceability;
+  late double? targetDanceability;
+  late int? minDurationMs;
+  late int? maxDurationMs;
+  late int? targetDurationMs;
+  late double? minEnergy;
+  late double? maxEnergy;
+  late double? targetEnergy;
+  late double? minInstrumentalness;
+  late double? maxInstrumentalness;
+  late double? targetInstrumentalness;
+  late int? minKey;
+  late int? maxKey;
+  late int? targetKey;
+  late double? minLiveness;
+  late double? maxLiveness;
+  late double? targetLiveness;
+  late double? minLoudness;
+  late double? maxLoudness;
+  late double? targetLoudness;
+  late int? minMode;
+  late int? maxMode;
+  late int? targetMode;
+  late int minPopularity;
+  late int maxPopularity;
+  late int? targetPopularity;
+  late double? minSpeechiness;
+  late double? maxSpeechiness;
+  late double? targetSpeechiness;
+  late double? minTempo;
+  late double? maxTempo;
+  late double? targetTempo;
+  late int? minTimeSignature;
+  late int? maxTimeSignature;
+  late int? targetTimeSignature;
+  late double? minValence;
+  late double? maxValence;
+  late double? targetValence;
 
   TracksRequest({
     this.seedGenres = const [],
@@ -154,7 +156,7 @@ class TracksRequest {
       'seed_genres': seedGenres.join(','),
       'seed_artists': seedArtists.join(','),
       'seed_tracks': seedTracks.join(','),
-      'limit': limit.toString(),
+      'limit': limit,
       'market': market,
       'min_acousticness': minAcousticness,
       'max_acousticness': maxAcousticness,
@@ -183,8 +185,8 @@ class TracksRequest {
       'min_mode': minMode,
       'max_mode': maxMode,
       'target_mode': targetMode,
-      'min_popularity': minPopularity.toString(),
-      'max_popularity': maxPopularity.toString(),
+      'min_popularity': minPopularity,
+      'max_popularity': maxPopularity,
       'target_popularity': targetPopularity,
       'min_speechiness': minSpeechiness,
       'max_speechiness': maxSpeechiness,
@@ -200,4 +202,98 @@ class TracksRequest {
       'target_valence': targetValence,
     };
   }
+
+  factory TracksRequest.fromJson(Map<String, dynamic> json) {
+    return TracksRequest(
+      seedGenres: json['seed_genres']?.split(',') ?? [],
+      seedArtists: json['seed_artists']?.split(',') ?? [],
+      seedTracks: json['seed_tracks']?.split(',') ?? [],
+      limit: json['limit'] ?? 20,
+      market: json['market'],
+      minAcousticness: json['min_acousticness']?.toDouble(),
+      maxAcousticness: json['max_acousticness']?.toDouble(),
+      targetAcousticness: json['target_acousticness']?.toDouble(),
+      minDanceability: json['min_danceability']?.toDouble(),
+      maxDanceability: json['max_danceability']?.toDouble(),
+      targetDanceability: json['target_danceability']?.toDouble(),
+      minDurationMs: json['min_duration_ms'],
+      maxDurationMs: json['max_duration_ms'],
+      targetDurationMs: json['target_duration_ms'],
+      minEnergy: json['min_energy']?.toDouble(),
+      maxEnergy: json['max_energy']?.toDouble(),
+      targetEnergy: json['target_energy']?.toDouble(),
+      minInstrumentalness: json['min_instrumentalness']?.toDouble(),
+      maxInstrumentalness: json['max_instrumentalness']?.toDouble(),
+      targetInstrumentalness: json['target_instrumentalness']?.toDouble(),
+      minKey: json['min_key'],
+      maxKey: json['max_key'],
+      targetKey: json['target_key'],
+      minLiveness: json['min_liveness']?.toDouble(),
+      maxLiveness: json['max_liveness']?.toDouble(),
+      targetLiveness: json['target_liveness']?.toDouble(),
+      minLoudness: json['min_loudness']?.toDouble(),
+      maxLoudness: json['max_loudness']?.toDouble(),
+      targetLoudness: json['target_loudness']?.toDouble(),
+      minMode: json['min_mode'],
+      maxMode: json['max_mode'],
+      targetMode: json['target_mode'],
+      minPopularity: ((json['min_popularity'] ?? 0) as num).toInt(),
+      maxPopularity: ((json['max_popularity'] ?? 100) as num).toInt(),
+      targetPopularity: json['target_popularity'],
+      minSpeechiness: json['min_speechiness']?.toDouble(),
+      maxSpeechiness: json['max_speechiness']?.toDouble(),
+      targetSpeechiness: json['target_speechiness']?.toDouble(),
+      minTempo: json['min_tempo']?.toDouble(),
+      maxTempo: json['max_tempo']?.toDouble(),
+      targetTempo: json['target_tempo']?.toDouble(),
+      minTimeSignature: json['min_time_signature'],
+      maxTimeSignature: json['max_time_signature'],
+      targetTimeSignature: json['target_time_signature'],
+      minValence: json['min_valence']?.toDouble(),
+      maxValence: json['max_valence']?.toDouble(),
+      targetValence: json['target_valence']?.toDouble(),
+    );
+  }
+}
+
+Map<String, dynamic> criteriaListToTrackRequestJson(
+    List<String>? seedGenres,
+    List<String>? seedArtists,
+    List<String>? seedTracks,
+    List<Criteria> criteriaList) {
+  Map<String, dynamic> jsonMap = {};
+
+  // Add seed genres, artists, and tracks if they are not null and contain elements
+  if (seedGenres != null && seedGenres.isNotEmpty) {
+    jsonMap['seed_genres'] = seedGenres.join(',');
+  }
+  if (seedArtists != null && seedArtists.isNotEmpty) {
+    jsonMap['seed_artists'] = seedArtists.join(',');
+  }
+  if (seedTracks != null && seedTracks.isNotEmpty) {
+    jsonMap['seed_tracks'] = seedTracks.join(',');
+  }
+
+  for (var criteria in criteriaList) {
+    // Check if values.start is different from min
+    if (criteria.values.start != criteria.min) {
+      jsonMap["min_${criteria.label.toLowerCase().replaceAll(" ", "_")}"] =
+          criteria.values.start;
+    }
+
+    // Check if values.end is different from max
+    if (criteria.values.end != criteria.max) {
+      jsonMap["max_${criteria.label.toLowerCase().replaceAll(" ", "_")}"] =
+          criteria.values.end;
+    }
+
+    // If a target value is provided (assuming a target is a specific value within the range), it can be added as well
+    // This assumes your Criteria class or the context where it's used allows identifying a 'target' value
+    if (criteria.target != null) {
+      jsonMap["target_${criteria.label.toLowerCase().replaceAll(" ", "_")}"] =
+          criteria.target;
+    }
+  }
+
+  return jsonMap;
 }
